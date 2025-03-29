6fab2d58d81bb32ce62b6d934fe810b4a233bf33
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-03-28 18:18:34 -0600

    docs: add comments

--- core/Controller.php
+++ core/Controller.php
@@ -1,8 +1,11 @@
 <?php
 
+/**
+ * Clase base para todos los controladores
+ */
 class Controller{
     public function view($view, $data = []){
         extract($data); // arreglo a variables
-        require_once "../app/views/$view.php";
+        require_once "../app/views/$view.php"; // cargar la vista
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -4,15 +4,23 @@ class Router
 {
     public function dispatch($url)
     {
+        // Eliminar el prefijo de la URL (si existe)
         $url = trim($url, '/');
         // Si la URL está vacía, usamos HomeController@index
         if ($url === '') {
+            // Redirigir a la página de inicio
             $controllerName = 'HomeController';
+
+            // Método por defecto
             $method = 'index';
         } else {
             // Separar la URL por "/"
             $parts = explode('/', $url);
+
+            // Asignar el controlador y el método
             $controllerName = ucfirst($parts[0]) . 'Controller';
+
+            // Si no se especifica el método, se usa "index" por defecto
             $method = isset($parts[1]) ? $parts[1] : 'index';
         }
 
@@ -20,8 +28,11 @@ class Router
         $controllerFile = "../app/controllers/$controllerName.php";
 
         if (file_exists($controllerFile)) {
+            // Incluir el archivo del controlador y crear una instancia
             require_once $controllerFile;
             $controller = new $controllerName();
+
+            // Verificar si el método existe en el controlador
             if (method_exists($controller, $method)) {
                 $controller->$method();
             } else {
