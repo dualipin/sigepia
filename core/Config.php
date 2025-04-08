cb1dc24c8292ad8f38923b9e6a396c863c829812
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-04-08 17:00:27 -0600

    fix env error

--- core/Config.php
+++ core/Config.php
@@ -2,19 +2,6 @@
 
 $initialRoute = getenv('INITIAL_ROUTE');
 
-echo $initialRoute;
-
-$databaseConfig = [
-    'host'     => getenv('DATABASE_HOST') ?: 'localhost',
-    'username' => getenv('DATABASE_USER') ?: 'root',
-    'password' => getenv('DATABASE_PASSWORD') ?: 'dualipin',
-    'database' => getenv('DATABASE_NAME') ?: 'sigepia',
-];
-
-define('DB_HOST', $databaseConfig['host']);
-define('DB_USERNAME', $databaseConfig['username']);
-define('DB_PASSWORD', $databaseConfig['password']);
-define('DB_DATABASE', $databaseConfig['database']);
 define('INITIAL_ROUTE', $initialRoute ? ($initialRoute === '/' ? '' : $initialRoute) : '');
 define('ASSETS_IMG', INITIAL_ROUTE . '/assets/img');
 
